a527a3b22efb8b2d1cdf8ff073b4aa43dbfa5edd
Blake <108043930+CyberNerdBlake@users.noreply.github.com>
2024-01-01 20:43:20 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import pyautogui
+import time
+
+print("#######################################################")
+print("                                                       ")
+print("                                                       ")
+print("                instagram flooder                      ")
+print("         https://github.com/CyberNerdBlake             ")
+print("                                                       ")
+print("                                                       ")
+print("                                                       ")
+print("                                                       ")
+print("                                                       ")
+print("#######################################################")
+print()
+
+
+k = "idc"
+while True:
+    pyautogui.typewrite(k)
+    pyautogui.press("Enter")
+    time.sleep(1)
+
